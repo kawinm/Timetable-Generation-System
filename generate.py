05f2b4aea16cd14efc60cf2b5cf821196168971a
Kawin <kawinstark@gmail.com>
2020-02-24 16:14:26 +0530

    Minor Change in Generation function
    Fixed bugs, Identified lot of bugs (Check boundary case), Need to improve code quality

--- generate.py
+++ generate.py
@@ -8,11 +8,16 @@ class Subject():
         self.span = span
 
 class Population():
-    def __init__(self):
+    def __init__(self,subject_obj_list):
         self.chromosome = []
+        self.subject_obj_list = subject_obj_list
+        total_periods = 15
+        number_of_subject = 5
 
         for i in range(100):
-            chromosome.append(Chromosome(subjects_obj_list, total_periods, number_of_subject))
+            self.chromosome.append(Chromosome(subject_obj_list, total_periods, number_of_subject))
+        
+        self.chromosome[0].printTable()
 
 
 class Chromosome():
@@ -25,21 +30,45 @@ class Chromosome():
 
         remaining_periods = self.total_periods
         while remaining_periods > 0:
-            r = random.randint(0,number_of_subject)
+            r = randint(0,number_of_subject-1)
             if subjects_obj_list[r].periods_per_week > 0:
                 for i in range(subjects_obj_list[r].span):
                     self.gene.append(Gene(subjects_obj_list[r].name))
                 remaining_periods -= 1
+            else:a
+                self.gene.append(Gene("N/A"))
 
     def getFitness(self):
         fitness = 1
         for i in range(self.total_periods):
+            pass
+
+    def printTable(self):
+        for i in range(self.total_periods):
+            print(self.gene[i].subject, end=" ")
+            if i% 7 == 0:
+                print()
 
 class Gene():
-    
+
     def __init__(self, subject):
         self.subject = subject
     
     def getSubject(self):
         return self.subject
 
+def main():
+    subject_name = ["OOAD","MC","IP","CD","ST"]
+    subject_hours = [3,3,3,3,3]
+
+    subject_obj_list = []
+    for i in range(5):
+        s = Subject(subject_name[i], subject_hours[i], 1)
+        subject_obj_list.append(s)
+    
+    for i in subject_obj_list:
+        print(i.name)
+
+    p = Population(subject_obj_list)
+
+main()
